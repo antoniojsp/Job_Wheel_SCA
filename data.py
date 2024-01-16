f805d3a114b304dfd7cce2dc0e89aef35aa01242
Antonio Silva Paucar <antonios@uoregon.edu>
2024-01-15 23:43:33 -0800

    job calendar

--- data.py
+++ data.py
@@ -1,50 +1,54 @@
 import pandas as pd
 from pprint import pprint
-
-# https://docs.google.com/spreadsheets/d/1XTb5lM1OOd4PNZ0O4FawsYIH1We6f-WH9lEkUV6yp0w/edit?usp=sharing
-SHEET_ID = '11trob6GMCskw1qbvILMPKBeJlKxTh_b_PfG9Iy80uVo'
-SHEET_NAME = 'WINTER_2024'
-url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-df = pd.read_csv(url)
-'''
-{"antonio":{"monday":["day dishes"]
-            "tuesday":["night dishes", "mop"]
-            "friday":["cook"]}
-'''
+from math import isnan
 
 
 class Jobs:
-    def __init__(self):
-        self.member = {}
+    def __init__(self, sheet_id: str, sheet_name: str):
+        self.members = {"No assigned jobs":[]}
+        self.sheet_id = sheet_id
+        self.sheet_name = sheet_name
+        self.pull_info_from_sheet()
 
-    def add_job(self, name: str, job: str, day: str, points:int):
-        if name not in self.member:
-            self.member[name] = {"points": 0}
+    def add_job(self, name: str, job: str, day: str, points: int):
+        if name not in self.members:
+            self.members[name] = {"points": 0}
 
-        if day not in self.member[name]:
-            self.member[name][day] = []
+        if day not in self.members[name]:
+            self.members[name][day] = []
 
-        self.member[name][day].append((job, points))
-        self.member[name]["points"] += points
+        self.members[name][day].append((job, points))
+        self.members[name]["points"] += points
 
-    def gather(self):
+    def no_assigned_jobs(self, job:str) -> None:
+        self.members["No assigned jobs"].append(job)
+
+    def pull_info_from_sheet(self) -> None:
+        url = f'https://docs.google.com/spreadsheets/d/{self.sheet_id}/gviz/tq?tqx=out:csv&sheet={self.sheet_name}'
+        df = pd.read_csv(url)
+
         for i in range(len(df)):
             job = df.values[i][0]
             day = df.values[i][1]
             name = df.values[i][5]
-            points = df.values[i][4]
+            points = float(df.values[i][4]) if not isnan(df.values[i][4]) else 0
+
             if isinstance(name, str):  # if there is no name, then do not add (for now)
                 name = name.capitalize()
                 if isinstance(day, str):
                     day = day.capitalize()
                 else:
-                    day = "Coord"
+                    day = "Coord"  # if it's not a day of the week or weekly/bi-weekly job, then is a coord job.
                 self.add_job(name.capitalize(), job.capitalize(), day.capitalize(), points)
+            else:
+                self.no_assigned_jobs((day, job, points))
+
 
     def dict(self):
-        return self.member
+        return self.members
 
 
-temp = Jobs()
-
+SHEET_ID = '11trob6GMCskw1qbvILMPKBeJlKxTh_b_PfG9Iy80uVo'
+SHEET_NAME = 'WINTER_2024'
+temp = Jobs(SHEET_ID, SHEET_NAME)
 pprint(temp.dict())
