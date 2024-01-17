d786c0c976ababd3695e39671af8e10cc84ac995
Antonio Silva Paucar <antonios@uoregon.edu>
2024-01-17 01:44:33 -0800

    almosty

--- data.py
+++ data.py
@@ -1,58 +1,71 @@
 import gspread
-import pandas as pd
 from pprint import pprint
-from math import isnan
 
 
 class Jobs:
-    def __init__(self, sheet_id: str, sheet_name: str):
-        self.members = {"No assigned jobs": []}
-        self.sheet_id = sheet_id
-        self.sheet_name = sheet_name
-        self.pull_info_from_sheet()
+    def __init__(self, credentials):
+        self.credentials = credentials
+        self.members = {"No assigned jobs": {"points": 0}} # starts by adding jobs that are not covered
+        self.pull_info_from_sheet(credentials)
 
-    def add_job(self, name: str, job: str, day: str, points: int):
+    def add_job(self, name: str, day: str, jobs_points: tuple[str, str]) -> None:
+        """
+        add a job, indicating the member assigned, the day, the name of the job and how
+        mamy points the member gets
+        :param name: name of member
+        :param day: day of the week, or if it's weekly, biweekly or coord
+        :param jobs_points:
+        :return:
+        """
         if name not in self.members:
             self.members[name] = {"points": 0}
 
         if day not in self.members[name]:
             self.members[name][day] = []
 
-        self.members[name][day].append((job, points))
-        self.members[name]["points"] += points
+        self.members[name][day].append(jobs_points)
+        self.members[name]["points"] += jobs_points[1]
 
-    def no_assigned_jobs(self, job: str) -> None:
-        self.members["No assigned jobs"].append(job)
+    def pull_info_from_sheet(self, credentials: str) -> None:
+        gc = gspread.service_account(filename=credentials)
 
-    def pull_info_from_sheet(self) -> None:
-        url = f'https://docs.google.com/spreadsheets/d/{self.sheet_id}/gviz/tq?tqx=out:csv&sheet={self.sheet_name}'
-        df = pd.read_csv(url)
+        sh = gc.open("Copy of JS Job Wheel")
+        sheet_names = [s.title for s in sh.worksheets()]
+        a = sh.worksheet(sheet_names[0])
 
-        for i in range(len(df)):
-            print(df)
-            job = df.values[i][0]
-            day = df.values[i][1]
-            name = df.values[i][5]
-            points = float(df.values[i][4]) if not isnan(df.values[i][4]) else 0
+        for row in a.get()[1:]:
+            length = len(row)
+            if length < 6:
+                row.append("")
 
-            if isinstance(name, str):  # if there is no name, then do not add (for now)
+            job = row[0] if row[0] else ""
+            day = row[1] if row[1] else ""
+            name = row[5] if row[5] else ""
+            points = row[4]
+            try:
+                points = float(points)
+            except ValueError:
+                points = 0
+
+            if len(name) > 0:  # if there is no name, then do not add (for now)
                 name = name.capitalize()
-                if isinstance(day, str):
-                    day = day.capitalize()
-                else:
-                    day = "Coord"  # if it's not a day of the week or weekly/bi-weekly job, then is a coord job.
-                self.add_job(name.capitalize(), job.capitalize(), day.capitalize(), points)
             else:
-                self.no_assigned_jobs((day, job, points))
+                name = "No assigned jobs"
+            if day:
+                day = day.capitalize()
+            else:
+                day = "Coord"  # if it's not a day of the week or weekly/bi-weekly job, then is a coord job.
+            job_points = (job, points)
+            self.add_job(name.capitalize(), day.capitalize(), job_points)
 
     def dict(self):
         return self.members
 
 
+a = Jobs("./credentials.json")
+pprint(a.dict())
+num = 0
+for i in a.dict():
+    num += a.dict()[i]["points"]
 
-
-
-SHEET_ID = '11trob6GMCskw1qbvILMPKBeJlKxTh_b_PfG9Iy80uVo'
-SHEET_NAME = 'WINTER_2024'
-temp = Jobs(SHEET_ID, SHEET_NAME)
-pprint(temp.dict())
+print(num)
